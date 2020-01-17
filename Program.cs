badd32bae9adc935493bc4d18bb218a91720e5d9
Nikifor <nikifor.nikiforovski@moneytech.com.au>
2020-01-17 11:53:30 +1100

    😁😁

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ArrayLenghtNegative
+{
+    class Program
+    {
+        public static unsafe void Main(string[] args)
+        {
+            int[] Array = new int[42];
+            fixed (int* p = Array)
+                p[-2] = -5;
+
+            Console.WriteLine(Array.Length);
+        }
+    }
+}
